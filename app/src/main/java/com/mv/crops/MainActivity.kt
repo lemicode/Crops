dcb31793debf29d6be668ba9c612cde2bdb62b70
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-16 16:27:49 -0500

    Splash screen

--- app/src/main/java/com/mv/crops/MainActivity.kt
+++ app/src/main/java/com/mv/crops/MainActivity.kt
@@ -20,6 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        Thread.sleep(3000)
+        setTheme(R.style.Theme_Crops)
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar()?.hide();
