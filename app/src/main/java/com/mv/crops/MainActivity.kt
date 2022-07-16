da2dced6c687fd6caf78b1a443986db3757a89ee
johnnyargote123 <alexco12@hotmail.es>
2022-07-16 11:09:40 -0500

    eliminar header purple

--- app/src/main/java/com/mv/crops/MainActivity.kt
+++ app/src/main/java/com/mv/crops/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -17,9 +18,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_main)
-
         val boton_login = findViewById<Button>(R.id.login_btn_iniciar_sesion)
         val txt_correo=findViewById<EditText>(R.id.login_txt_usuario)
         val txt_contrasena=findViewById<EditText>(R.id.login_txt_contrasena)
