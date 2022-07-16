9978c2100ce75f03600895f2ae8fcf3d624d63ae
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-16 14:41:43 -0500

    Se modifica el activity de carga

--- app/src/main/java/com/mv/crops/LoadActivity.kt
+++ app/src/main/java/com/mv/crops/LoadActivity.kt
@@ -1,8 +1,11 @@
 package com.mv.crops
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import java.util.Timer
 import kotlin.concurrent.schedule
 
@@ -11,9 +14,16 @@ class LoadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load)
+
+        Log.d(ContentValues.TAG,"listo")
+
         Timer("SettingUp", false).schedule(3000) {
             changeActivity()
         }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return super.onKeyDown(keyCode, event)
     }
 
     fun changeActivity() {
