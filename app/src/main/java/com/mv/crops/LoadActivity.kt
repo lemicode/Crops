cd1f892238c6234b909d2fb3d307d0fa3b3f7614
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-16 14:13:15 -0500

    Se modifica el activity de carga

--- app/src/main/java/com/mv/crops/LoadActivity.kt
+++ app/src/main/java/com/mv/crops/LoadActivity.kt
@@ -3,25 +3,17 @@ package com.mv.crops
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import com.google.android.gms.tasks.Tasks.withTimeout
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class LoadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load)
-
-        val timer = object: CountDownTimer(3000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                return
-            }
-            override fun onFinish() {
-                changeActivity();
-            }
+        Timer("SettingUp", false).schedule(3000) {
+            changeActivity()
         }
-        timer.start()
-
     }
 
     fun changeActivity() {
