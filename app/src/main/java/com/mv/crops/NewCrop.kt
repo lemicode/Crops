2d4566344f0faef8ba93d4f49093768d0a2cb2be
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-16 17:07:43 -0500

    Se crea formulario agregar cultivo

---
+++ app/src/main/java/com/mv/crops/NewCrop.kt
@@ -0,0 +1,11 @@
+package com.mv.crops
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCrop : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_crop)
+    }
+}
