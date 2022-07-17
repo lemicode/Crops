c1bd1925ba5aa866868bc4c6699b7c3272a9edd1
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-16 21:43:20 -0500

    Formulario agregar cultivos ya guarda datos

--- app/src/main/java/com/mv/crops/NewCropActivity.kt
+++ app/src/main/java/com/mv/crops/NewCropActivity.kt
@@ -1,6 +1,7 @@
 package com.mv.crops
 
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,38 +20,33 @@ class NewCropActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar()?.hide();
         setContentView(R.layout.activity_new_crop)
-
         val txt_ubicacion = findViewById<EditText>(R.id.new_crop_txt_ubicacion)
         val txt_nombre_cultivo = findViewById<EditText>(R.id.new_crop_txt_nombre_cultivo)
         val txt_area_cultivo = findViewById<EditText>(R.id.new_crop_txt_area_cultivo)
         val txt_fecha_inicio = findViewById<EditText>(R.id.new_crop_txt_fecha_inicio)
         val boton_anadir = findViewById<Button>(R.id.new_crop_btn_anadir)
-
         val boton_regresar=findViewById<ImageView>(R.id.new_crop_btn_regresar)
-
         boton_regresar.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
         boton_anadir.setOnClickListener {
             try {
-
-                val ubicacion = txt_ubicacion.text.toString().lowercase()
-                val nombre_cultivo = txt_nombre_cultivo.text.toString().lowercase()
+                val ubicacion = txt_ubicacion.text.toString()
+                val nombre_cultivo = txt_nombre_cultivo.text.toString()
                 val area_cultivo = txt_area_cultivo.text.toString()
                 val fecha_inicio = txt_fecha_inicio.text.toString()
-
-                db.collection("crops").document(nombre_cultivo).set{
+                db.collection("crops").document(nombre_cultivo).set(
                     hashMapOf(
-                        "ubicacion" to 1
+                        "ubicacion" to ubicacion,
+                        "area" to area_cultivo,
+                        "fecha_inicio" to fecha_inicio
                     )
-                }
-
+                )
             } catch (e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             }
-
         }
-
     }
+
 }
