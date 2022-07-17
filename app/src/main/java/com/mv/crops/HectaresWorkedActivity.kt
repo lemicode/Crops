8fba0127e3e28f8a6849f4ea67923a066226f11c
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-16 20:01:55 -0500

    Se añadieron 3 formularios

--- app/src/main/java/com/mv/crops/HectaresWorkedActivity.kt
+++ app/src/main/java/com/mv/crops/HectaresWorkedActivity.kt
@@ -6,19 +6,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-class HoursWorkedActivity : AppCompatActivity() {
+class HectaresWorkedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hours_worked)
+        setContentView(R.layout.activity_hectares_worked)
 
-        val txt_tiempo_trabajado = findViewById<EditText>(R.id.hours_worked_txt_time_worked)
-        val txt_fecha = findViewById<EditText>(R.id.hours_worked_txt_fecha)
-        val boton_anadir = findViewById<Button>(R.id.hours_worked_btn_anadir)
+        val txt_hectareas_trabajadas = findViewById<EditText>(R.id.hectares_worked_txt_amount)
+        val boton_anadir = findViewById<Button>(R.id.hectares_worked_btn_anadir)
 
         boton_anadir.setOnClickListener {
             try {
-                val tiempo_trabajado = txt_tiempo_trabajado.text.toString()
-                val fecha = txt_fecha.text.toString()
+                val hectareas_trabajadas = txt_hectareas_trabajadas.text.toString()
             } catch (e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             }
