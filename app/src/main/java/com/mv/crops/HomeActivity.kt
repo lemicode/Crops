55e2ae6ba1a8f1a4e860cddedc14ce9406071610
johnnyargote123 <alexco12@hotmail.es>
2022-07-16 19:20:42 -0500

    ajuste imagenes home 2

--- app/src/main/java/com/mv/crops/HomeActivity.kt
+++ app/src/main/java/com/mv/crops/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.mv.crops
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,8 @@ class HomeActivity : AppCompatActivity() {
         setTheme(R.style.Theme_Crops)
 
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_home)
     }
 }
