20acd754ce966b6fe774786ddfd4dc496ec258a2
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-07-17 10:33:10 -0500

    Se crea el DatePicker para formulario agregar cultivos

--- app/src/main/java/com/mv/crops/NewCropActivity.kt
+++ app/src/main/java/com/mv/crops/NewCropActivity.kt
@@ -17,19 +17,28 @@ class NewCropActivity : AppCompatActivity() {
     private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar()?.hide();
+
         setContentView(R.layout.activity_new_crop)
+
         val txt_ubicacion = findViewById<EditText>(R.id.new_crop_txt_ubicacion)
         val txt_nombre_cultivo = findViewById<EditText>(R.id.new_crop_txt_nombre_cultivo)
         val txt_area_cultivo = findViewById<EditText>(R.id.new_crop_txt_area_cultivo)
         val txt_fecha_inicio = findViewById<EditText>(R.id.new_crop_txt_fecha_inicio)
         val boton_anadir = findViewById<Button>(R.id.new_crop_btn_anadir)
         val boton_regresar=findViewById<ImageView>(R.id.new_crop_btn_regresar)
+
+        txt_fecha_inicio.setOnClickListener{
+            showDatePickerDialog()
+        }
+
         boton_regresar.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
+
         boton_anadir.setOnClickListener {
             try {
                 val ubicacion = txt_ubicacion.text.toString()
@@ -47,6 +56,15 @@ class NewCropActivity : AppCompatActivity() {
                 Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment{ day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    fun onDateSelected(day: Int, month: Int, year: Int) {
+        findViewById<EditText>(R.id.new_crop_txt_fecha_inicio).setText("${day}/${month + 1}/${year}")
     }
 
 }
